287460ad2617fedd2bea931d31f4b0ab57f0692b
zhangying14 <zhangying14@zuoyebang.com>
2022-06-01 21:38:12 +0800

    update

--- flink/flink_1_15/src/main/scala/cn/izualzhy/JustTest.scala
+++ flink/flink_1_15/src/main/scala/cn/izualzhy/JustTest.scala
@@ -1,15 +1,6 @@
 package cn.izualzhy
 
-/**
- * Author: zhangying14
- * Date: 2022/5/26 14:58
- * Package: cn.izualzhy
- * Description:
- *
- */
 object JustTest extends App {
-  val s = "Hello World"
-  val t = s.split(".")
-  println(t.mkString(","))
+  println(System.getProperty("java.io.tmpdir"))
 
 }
--- flink/flink_1_15/src/main/scala/cn/izualzhy/LiteralInRowTest.scala
+++ flink/flink_1_15/src/main/scala/cn/izualzhy/LiteralInRowTest.scala
@@ -4,20 +4,21 @@ import org.apache.flink.streaming.api.scala._
 import org.apache.flink.table.api.bridge.scala.StreamTableEnvironment
 
 object LiteralInRowTest extends App {
-  val env = StreamExecutionEnvironment.getExecutionEnvironment
+  val env = StreamExecutionEnvironment.createLocalEnvironment(1)
   val tEnv = StreamTableEnvironment.create(env)
 
-  val source = env.fromElements("a")
-  val source_table = tEnv.fromDataStream(source).as("a")
+  val source = env.fromElements("test_field")
+  val source_table = tEnv.fromDataStream(source).as("f0")
 
   tEnv.createTemporaryView(
     "source_table",
     source_table)
-  val sql = "SELECT Row('literal_column', a) FROM source_table"
+  val sql = "SELECT Row(f0, 'literal_column', 'another_literal_column') FROM source_table"
+//  val sql = "SELECT Row('literal_column', real_column, 'another_literal_column') FROM source_table"
   /*
   Exception in thread "main" org.apache.flink.table.api.SqlParserException: SQL parse failed. Encountered "\'literal_column\'" at line 1, column 15.
    */
-//  val error_sql = "SELECT Row(a, 'literal_column') FROM source_table"
+//  val sql = "SELECT Row(real_column, 1) FROM source_table"
   tEnv.executeSql(sql).print()
 
 }
