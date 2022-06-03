b1931bdd0c1f0d6bc543cc21bc20c45113c99480
zhangying14 <zhangying14@zuoyebang.com>
2022-06-03 10:48:57 +0800

    update

--- flink/flink_1_15/src/main/scala/cn/izualzhy/JustTest.scala
+++ flink/flink_1_15/src/main/scala/cn/izualzhy/JustTest.scala
@@ -1,6 +1,4 @@
 package cn.izualzhy
 
 object JustTest extends App {
-  println(System.getProperty("java.io.tmpdir"))
-
 }
--- flink/flink_1_15/src/main/scala/cn/izualzhy/LiteralInRowTest.scala
+++ flink/flink_1_15/src/main/scala/cn/izualzhy/LiteralInRowTest.scala
@@ -7,18 +7,13 @@ object LiteralInRowTest extends App {
   val env = StreamExecutionEnvironment.createLocalEnvironment(1)
   val tEnv = StreamTableEnvironment.create(env)
 
-  val source = env.fromElements("test_field")
+  val source = env.fromElements("test_f0_value")
   val source_table = tEnv.fromDataStream(source).as("f0")
 
   tEnv.createTemporaryView(
     "source_table",
     source_table)
-  val sql = "SELECT Row(f0, 'literal_column', 'another_literal_column') FROM source_table"
-//  val sql = "SELECT Row('literal_column', real_column, 'another_literal_column') FROM source_table"
-  /*
-  Exception in thread "main" org.apache.flink.table.api.SqlParserException: SQL parse failed. Encountered "\'literal_column\'" at line 1, column 15.
-   */
-//  val sql = "SELECT Row(real_column, 1) FROM source_table"
+  val sql = "SELECT Row(f0, 'literal_column') FROM source_table"
+//  val sql = "SELECT Row('literal_column', f0) FROM source_table"
   tEnv.executeSql(sql).print()
-
 }
